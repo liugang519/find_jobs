a1d84ab3e2457ef3a12f9eb2765d9eba88bc6725
liugang519 <liugang519@163.com>
2014-03-15 22:52:04 +0800

    fix bug

--- find_jobs_server.py
+++ find_jobs_server.py
@@ -11,7 +11,7 @@ import redis
 
 from tornado.options import define, options
 from spider_conf import *
-from server_conf.py import *
+from server_conf import *
 
 define("port", default=8888, help="run on the given port", type=int)
 
@@ -23,7 +23,7 @@ class MainHandler(tornado.web.RequestHandler):
         id_list = rs.zrevrange("index_time_sset", 1, -1)
         jobs_list = []
         if p == "none":
-            for id in xrange(INDEX_NUMBER)
+            for id in xrange(INDEX_NUMBER):
                 jobs_list.append(rs.hgetall("article:ParttimeJob:"+id))
         else:
             p = int(p)
@@ -40,7 +40,8 @@ class MainHandler(tornado.web.RequestHandler):
             url_pre = "/?p="+str(p-1)
         if (p*INDEX_NUMBER+1) <= len(id_list):
             url_next = "/?p="+str(p+1)
-         self.render("index.html", jobs_list=jobs_list, url_pre=url_pre, url_next=url_next)
+        
+        self.render("index.html", jobs_list=jobs_list, url_pre=url_pre, url_next=url_next)
             
 def main():
     tornado.options.parse_command_line()
