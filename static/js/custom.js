94d6e9987a9d6f5ce91cd9ab21e8e192b2ee5821
sue891206 <sue_song@126.com>
2014-03-22 23:12:56 +0800

    delete unused title pre

--- static/js/custom.js
+++ static/js/custom.js
@@ -5,12 +5,50 @@ for use in find_jobs
 author: mm
  */
 $(document).ready(function() {
-    if (document.title == "Find Jobs")
-        $("li#home").addClass("active");
 
-    if ($("#page_up > a").attr("href") == "")
-        $("#page_up").addClass("disabled");
+    function check_new_line(obj_article) {
+        var article = obj_article.text();
+        obj_article.html(article.replace(/\n/g, "<br />"));
+    } // transform '\n' to '<br />'
 
-    if ($("#page_down > a").attr("href") == "")
-        $("#page_down").addClass("disabled");
+    function delete_useless_title(obj_title) {
+        obj_title.text(obj_title.text().slice(3));
+    }
+
+    function delete_home_list_title(obj_list) {
+        obj_list.children().each(function() {
+            /* iterate through array or object */
+            delete_useless_title($(this).children().first());
+        });
+    }
+
+    $('#home a').click(function(e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    $('#about a').click(function(e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+
+    if (document.title == "Find Jobs") {
+        $("#home").addClass("active");
+
+        if ($("#page_up > a").attr("href") == "")
+            $("#page_up").addClass("disabled");
+
+        if ($("#page_down > a").attr("href") == "")
+            $("#page_down").addClass("disabled");
+
+        delete_home_list_title($("div.list-group"));
+    }
+
+
+    if (document.title == "Job Details") {
+        check_new_line($("div.panel-body > p"));
+        delete_useless_title($("h3#article-title"));
+    }
+
+
+
 });
