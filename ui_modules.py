5d9ba7b8acd3a6b78f1b60b1c6db6d2ef11fa989
sue891206 <sue_song@126.com>
2014-05-18 11:18:27 +0800

    add ui modules

---
+++ ui_modules.py
@@ -0,0 +1,12 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+class HeaderModule(tornado.web.UIModule):
+    def render(self):
+        return self.render_string("header.html")
+
+class FooterModule(tornado.web.UIModule):
+    def render(self):
+        return self.render_string("footer.html")
